ea31db105db2ba3f2526fdebb7064a402b1d9ef6
Mas Tsabio <prinztsabioprabhudanishwara@gmail.com>
2025-11-27 10:54:47 +0700

    Bootstrap Version

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log(document.title);
+
+document.title = "Prinz Portofolio";
+
+console.log(document.title);
+
+const body = document.body;
+
+const h1 = document.createElement("marquee");// <h1></h1>
+h1.textContent = "WELCOME TO MY PORTOFOLIO" // <h1>Belajar Javascript
+body.appendChild(h1)
+
